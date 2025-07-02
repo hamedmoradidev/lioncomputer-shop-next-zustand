eb1db1b26b0f9d50e55198255f1961db48e503da
Hamed Moradi <123096117+hamedmoradidev@users.noreply.github.com>
2025-07-02 11:03:06 +0330

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ['mmwlixakfkxorqpgsqya.supabase.co'],
-        unoptimized: true,
-    },    
 };
 
 export default nextConfig;
